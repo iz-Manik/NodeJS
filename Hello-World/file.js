8c47c056a7f36335b7f08fe9c342bebc39977cb3
Manik <manikchauhan029@gmail.com>
2025-01-11 10:11:28 +0530

    nodeJS working

--- Hello-World/file.js
+++ Hello-World/file.js
@@ -1,4 +1,5 @@
 const fs=require("fs");
+const os=require("os");
 //Sync
 fs.writeFileSync("./test.txt","Hey There");
 
@@ -7,4 +8,24 @@ fs.writeFile("./test.txt","Hey There",()=>{
     console.log("File Written");
 });
 
-fs.readFileSync("./test.txt","utf8");
+const result=fs.readFileSync("./test.txt","utf8");
+console.log(result);
+
+fs.readFile("./test.txt","utf8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(data);
+});
+
+fs.appendFileSync("./test.txt","Hello World");
+
+//to copy file to another location
+fs.cpSync("./test.txt","./test2.txt");
+
+//to delete file
+fs.rmSync("./test.txt");
+
+//default thread pool size is 4
+//we can extend thread pool size to maximum number of cores available
+console.log(os.cpus().length);
