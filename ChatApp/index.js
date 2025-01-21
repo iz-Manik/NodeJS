72ddb175d38b1c86e5109335f6b195e0d605167d
Manik <manikchauhan029@gmail.com>
2025-01-22 03:46:56 +0530

    ChatApp

--- ChatApp/index.js
+++ ChatApp/index.js
@@ -1,29 +1,23 @@
-const http=require('http');
-const express=require('express');
-const {Server}=require('socket.io');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
-const app=express();
-const server=http.createServer(app);
-const path=require('path');
-const io=new Server(server);
-const PORT=8000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-//Socket.io
-
-io.on('connection',(socket)=>{
-    console.log('User connected',socket.id);
-    socket.on('disconnect',()=>{
-        console.log('User disconnected');
-    });
-    socket.on('chat message',(msg)=>{
-        io.emit('chat message',msg);
-    });
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
 });
 
+app.use(express.static(path.resolve("./public")));
 
-app.use(express.static(path.resolve('./public')));
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
 
-app.get('/',(req,res)=>{
-    return res.sendFile('/public/index.html')
-})
-server.listen(PORT,()=>{console.log(`Server is running on port ${PORT}`)});
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
